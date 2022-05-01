40e08cfecee927f8fd29ead8e7e44e0bc9446b71
vibqetowi <abutcher.usa@protonmail.com>
2022-04-30 23:56:52 -0400

    v

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,8 @@
+import data from "./data";
+
 function App() {
   return (
     <>
-      <div className="home" id="home">
-        <div className="home-text">
-          <h1 className="section-title">Fresh and bio grocery store</h1>
-          <p>
-            Start eating better today by browsing our vast selection of fresh
-            local produce
-          </p>
-        </div>
-      </div>
-
       <section>
         <h1 className="section-title">
           our <span>featured</span>products !
@@ -50,72 +42,20 @@ function App() {
           Our<span>aisles</span>
         </h1>
         <ul className="aisle-group">
-          <li className="aisle">
-            <a href="./p2_fruitaisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img
-                src="./frontend/images/Fruits.jpg"
-                alt="fruitaisle"
-                classNameName="rand"
-              ></img>
-              <h2 className="aisle-text">Fruits</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_VegetablesAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/vegetables.jpg" alt=""></img>
-              <h2 className="aisle-text">Vegetables </h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_DairyAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Dairy.jpg" alt=""></img>
-              <h2 className="aisle-text">Dairy</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_MeatAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Meat.jpg" alt=""></img>
-              <h2 className="aisle-text">Meat</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_PastryAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Pastry.jpg" alt=""></img>
-              <h2 className="aisle-text">Pastry</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_SnacksAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Snacks.jpg" alt=""></img>
-              <h2 className="aisle-text">Packaged Goods</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_SeafoodAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Seafood.jpg" alt=""></img>
-              <h2 className="aisle-text">Seafood</h2>
-            </a>
-          </li>
-          <li className="aisle">
-            <a href="./p2_WaterAisle.php">
-              <i className="bx bx-chevron-right icon"></i>
-              <img src="./images/Water.jpg" alt=""></img>
-              <h2 className="aisle-text">Drinks</h2>
-            </a>
-          </li>
+          {data.aisles.map((aisles) => (
+            <div key={aisles._i} className="ca2rd">
+              <li className="aisle">
+                <a href={`/aisles/$(aisles._id}`}>
+                  <i className="bx bx-chevron-right icon"></i>
+                  <img src={aisles.image} alt={aisles.name}></img>
+                  <h2 className="aisle-text">{aisles.name}</h2>
+                </a>
+              </li>
+            </div>
+          ))}
         </ul>
       </section>
-
-
     </>
   );
 }
-
 export default App;
